b941730e8fab91040533c2bc28c7c51cfa2156ec
fpy94 <fengpeiyuan94@gmail.com>
2020-04-29 19:57:49 +0800

    first commit

---
+++ build_tree.py
@@ -0,0 +1,84 @@
+import ete3
+from ete3 import Tree
+import numpy as np
+import argparse
+import torch
+import os
+import pickle
+from torch.utils.data import Dataset, DataLoader,TensorDataset
+from dataset import *
+from CNNpan import CNNpan
+from CNNpan_DBT import CNNpan_DBT
+def BTQuery(q,tree,trainfeature,k=10):
+    currentname=tree.get_tree_root().name
+    while True:
+        children=tree.search_nodes(name=currentname)[0].get_children()
+        if len(children)<k:
+            children.append(tree.search_nodes(name=currentname)[0])
+        dmin=np.inf
+        minnode=None
+        for c in children:
+            name=c.name
+            d=np.sum((trainfeature[q]-trainfeature[name])**2)
+            if d<dmin:
+                dmin=d
+                minnode=c.name
+        if minnode==currentname:
+            break
+        currentname=minnode
+    return currentname
+def BTtrain(trainfeature,label,eps=0.1):
+    Nnodes=trainfeature.shape[0]
+    Nodelist=np.random.permutation(Nnodes)
+    tree=Tree(name=Nodelist[0])
+    querynum=1
+    nodenum=1
+    for nodeidx in Nodelist[1:]:
+        vmin=BTQuery(nodeidx,tree,trainfeature)
+        if np.abs(label[vmin]-label[nodeidx])>eps:
+#             tree.search_nodes(name=vmin)[0].add_child(name=nodeidx,dist=np.abs(label[vmin]-label[nodeidx]))
+            tree.search_nodes(name=vmin)[0].add_child(name=nodeidx)
+            nodenum+=1
+        querynum+=1
+        if querynum%2500==0:
+            print('querynum {}, treenum {}'.format(querynum,nodenum))
+    print('tree building finished, treenum {}'.format(nodenum))
+    return tree,Nodelist
+
+def build_tree(cv,bs,modeldir,savedir):
+    with open('cvdata.pkl','rb') as f:
+        data=pickle.load(f)
+    train_data,test_data=data[cv]
+    train_loader=DataLoader(allele_dataset(train_data),batch_size=bs,shuffle=False)
+    net=CNNpan_DBT()
+    net.cuda()
+    checkpoint=torch.load(modeldir)
+    net.load_state_dict(checkpoint['model_state_dict'])
+    epoch=checkpoint['epochs']
+    print('model checkpoint {}'.format(epoch))
+    trainfeature=[]
+    trainlabel=[]
+    for batch_idx,(sx,mx,y) in enumerate(train_loader):
+        trainfeature.append(net.build(sx,mx,y))
+        trainlabel.append(y)
+    trainfeature=np.concatenate(trainfeature,0)
+    trainlabel=np.concatenate(trainlabel,0)
+    print(trainfeature.shape,trainlabel.shape)
+    tree,nodelist=BTtrain(trainfeature,trainlabel,0.1)
+    with open(os.path.join(savedir,'tree_cv_'+str(cv)+'_epoch_'+str(epoch)+'.pkl'), 'wb') as f:
+        pickle.dump({'tree':tree,'nodelist':nodelist},f)
+    
+if __name__=='__main__':
+    parser=argparse.ArgumentParser()
+    parser.add_argument('-gpu','--gpu',default="3",type=str,help='#gpu')
+    parser.add_argument('-savedir','--save_dir',default=None,type=str,help='saving dir')
+    parser.add_argument('-mpath','--model_path',default=None,type=str,help='continuning training path')
+    parser.add_argument('-cv','--cv',type=int,help='cross validation index')
+    parser.add_argument('-bs','--batch_size',default=1000,type=int,help='batch size in which a half is used to build tree and the other is used to query the model')
+    args=parser.parse_args()
+    os.environ["CUDA_VISIBLE_DEVICES"] = args.gpu
+    if not os.path.exists(args.save_dir):
+        os.makedirs(args.save_dir)
+    build_tree(args.cv,args.batch_size,args.model_path,args.save_dir)
+
+
---
+++ dataset.py
@@ -0,0 +1,141 @@
+import numpy as np
+import torch
+import torch.nn as nn
+import pickle
+import random
+from torch.utils.data import Dataset, DataLoader,TensorDataset
+from sklearn.model_selection import KFold
+
+			
+
+class allele_dataset:
+	def __init__(self,data):
+		seq,soh,mhcoh,score,allele=zip(*data)
+		self.sx=np.concatenate([np.expand_dims(i,0) for i in soh],0)
+		self.sx=self.sx.astype(np.float32)
+		self.sx/=10
+		self.mx=np.concatenate([np.expand_dims(i,0) for i in mhcoh],0)
+		self.mx=self.mx.astype(np.float32)
+		self.mx/=10
+
+		self.y=np.expand_dims(np.array(score),1)
+		self.y=self.y.astype(np.float32)
+	def __getitem__(self,i):
+		return self.sx[i],self.mx[i],self.y[i]
+	def __len__(self):
+		return len(self.sx)
+
+class meta_dataset:
+	def __init__(self,data,allele_list,n_support,n_query):
+		self.data=data
+		self.allele_list=allele_list
+		self.n_support=n_support
+		self.n_query=n_query      
+		self.batch_size=self.n_support+self.n_query
+		
+		self.dataset=[]
+		for key in self.allele_list:
+			if key in self.data:
+				self.dataset.append(DataLoader(allele_dataset(self.data[key]),batch_size=self.batch_size,shuffle=True))
+		
+	def __getitem__(self,i):
+		return next(iter(self.dataset[i]))
+	def __len__(self):
+		return len(self.dataset)
+
+
+class EpisodicBatchSampler(object):
+	def __init__(self,dataset,n_way,n_episode):
+		self.n_episode=n_episode
+		self.n_way=n_way
+		self.allele_num=len(dataset)
+	def __len__(self):
+		return self.n_episode
+	def __iter__(self):
+		for i in range(self.n_episode):
+			yield torch.randperm(self.allele_num)[:self.n_way]
+class Meta_Loader():
+	def __init__(self,datafile,allelelist,batch_size,n_support,n_query,n_episode=100,mode='train'):
+		self.mode=mode
+		self.batch_size=batch_size
+		self.n_support=n_support
+		self.n_query=n_query
+		self.n_episode=n_episode
+		self.allelelist=allelelist
+		with open(datafile,'rb') as f:
+			self.data=pickle.load(f)['data']
+	def get_data_loader(self):
+		metad=meta_dataset(self.data,self.allelelist,self.n_support,self.n_query)
+		sampler=EpisodicBatchSampler(metad,self.batch_size,self.n_episode)
+		if self.mode=='test':
+			data_loader=DataLoader(metad,batch_size=1,shuffle=False)
+		else:
+			data_loader=DataLoader(metad,batch_sampler=sampler)
+		return data_loader
+
+class Meta_Loader_4rs():
+	def __init__(self,data,allelelist,batch_size,n_support,n_query,n_episode=100,mode='train'):
+		self.mode=mode
+		self.batch_size=batch_size
+		self.n_support=n_support
+		self.n_query=n_query
+		self.n_episode=n_episode
+		self.allelelist=allelelist
+		self.data=data
+	def get_data_loader(self):
+		metad=meta_dataset(self.data,self.allelelist,self.n_support,self.n_query)
+		sampler=EpisodicBatchSampler(metad,self.batch_size,self.n_episode)
+		if self.mode=='test':
+			data_loader=DataLoader(metad,batch_size=1,shuffle=False)
+		else:
+			data_loader=DataLoader(metad,batch_sampler=sampler)
+		return data_loader
+
+class allele_dataset_single:
+	def __init__(self,data):
+		seq,soh,score=zip(*data)
+		self.sx=np.concatenate([np.expand_dims(i,0) for i in soh],0)
+		self.sx=self.sx.astype(np.float32)
+		self.sx/=10
+
+		self.y=np.expand_dims(np.array(score),1)
+		self.y=self.y.astype(np.float32)
+	def __getitem__(self,i):
+		return self.sx[i],self.y[i]
+	def __len__(self):
+		return len(self.sx)
+class meta_dataset_single:
+	def __init__(self,data,allele_list,n_support,n_query):
+		self.data=data
+		self.allele_list=allele_list
+		self.n_support=n_support
+		self.n_query=n_query      
+		self.batch_size=self.n_support+self.n_query
+		
+		self.dataset=[]
+		for key in self.allele_list:
+			if key in self.data:
+				self.dataset.append(DataLoader(allele_dataset_single(self.data[key]),batch_size=self.batch_size,shuffle=True))
+		
+	def __getitem__(self,i):
+		return next(iter(self.dataset[i]))
+	def __len__(self):
+		return len(self.dataset)
+class Meta_Loader_4rs_single():
+	def __init__(self,data,allelelist,batch_size,n_support,n_query,n_episode=100,mode='train'):
+		self.mode=mode
+		self.batch_size=batch_size
+		self.n_support=n_support
+		self.n_query=n_query
+		self.n_episode=n_episode
+		self.allelelist=allelelist
+		self.data=data
+	def get_data_loader(self):
+		metad=meta_dataset_single(self.data,self.allelelist,self.n_support,self.n_query)
+		sampler=EpisodicBatchSampler(metad,self.batch_size,self.n_episode)
+		if self.mode=='test':
+			data_loader=DataLoader(metad,batch_size=1,shuffle=False)
+		else:
+			data_loader=DataLoader(metad,batch_sampler=sampler)
+		return data_loader
+
